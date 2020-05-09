ae257e5ceae4b672e3148d54dcc061898ee52ff4
trunghieumickey <tunglam250@gmail.com>
2020-05-09 13:29:55 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="colorprint3", # Replace with your own username
+    name="colorprint3",
     version="1.0.0",
     author="trunghieumickey",
     author_email="trunghieumickey@outlook.com.vn",
