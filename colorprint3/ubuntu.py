bf1089812e0f77b6d9e1762bd3ac57c8577f0a63
trunghieumickey <tunglam250@gmail.com>
2020-05-14 22:37:13 +0700

    Update ubuntu.py

--- colorprint3/ubuntu.py
+++ colorprint3/ubuntu.py
@@ -1,6 +1,6 @@
 from builtins import print as pythonprint
 from os import system
-def print(*word,sep=None,end=None,file=None,flush=True,color="white"):
+def print(*word,sep=None,end=None,file=None,flush=True,color="white",background="black"):
     dic = {
         "black": "0",
         "red": "1",
@@ -11,8 +11,9 @@ def print(*word,sep=None,end=None,file=None,flush=True,color="white"):
         "cyan": "6",
         "white": "7"
     }
-    if (color in dic) and flush:
-        system("/bin/echo -ne \"\\e[0;3"+str(dic[color])+"m\"")
+    if (color in dic) and (background in dic) and flush:
+        system("/bin/echo -ne \"\\e[3"+str(dic[color])+"m\"")
+        system("/bin/echo -ne \"\\e[4"+str(dic[background])+"m\"")
         pythonprint(*word,sep=sep,end=end,file=file,flush=flush)
         system("/bin/echo -ne \"\\e[0m\"")
     else :
