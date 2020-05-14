1ef50742b53d0c87af15fc7f5edb8c0f6f5796b8
trunghieumickey <tunglam250@gmail.com>
2020-05-14 22:37:52 +0700

    Update windows.py

--- colorprint3/windows.py
+++ colorprint3/windows.py
@@ -1,6 +1,6 @@
 from builtins import print as pythonprint
 from os import system
-def print(*word,sep=None,end=None,file=None,flush=True,color="white"):
+def print(*word,sep=None,end=None,file=None,flush=True,color="white",background="black"):
     dic = {
         "black": "0",
         "red": "1",
@@ -11,8 +11,9 @@ def print(*word,sep=None,end=None,file=None,flush=True,color="white"):
         "cyan": "6",
         "white": "7"
     }
-    if (color in dic) and flush:
+    if (color in dic) and (background in dic) and flush:
         system('echo|set /p=[3'+str(dic[color])+'m')
+        system('echo|set /p=[4'+str(dic[background])+'m')
         pythonprint(*word,sep=sep,end=end,file=file,flush=flush)
         system('echo|set /p=[0m')
     else :
